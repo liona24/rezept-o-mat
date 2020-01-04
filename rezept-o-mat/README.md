d821ec57ea1b44073f182f8152fb88cb22eec18f
Lion Ackermann <ackerm.lion@hotmail.de>
2020-01-04 18:27:56 +0100

    Fuck nuxtjs

---
+++ rezept-o-mat/README.md
@@ -0,0 +1,28 @@
+# rezept-o-mat
+
+## Project Setup
+
+```bash
+
+# Fire up a environment if desired
+$ docker run -it -v $(pwd):/var/rezept-o-mat -p 8080:8080 --user=node node /bin/bash
+
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+
+```
+
+By default, the recipes are fetched from a mock JSON server. The URL can be configured in [src/api.js](src/api.js)
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- rezept-o-mat/src/App.vue
+++ rezept-o-mat/src/App.vue
@@ -51,7 +51,8 @@
 </template>
 
 <script>
-import { ENDPOINT_RECIPE } from '~/assets/api'
+import { ENDPOINT_RECIPE } from "./api";
+import axios from "axios";
 
 export default {
   data() {
@@ -64,37 +65,36 @@ export default {
         description: [],
         source: null
       }
-    }
+    };
   },
   created() {
-    this.reload()
+    this.reload();
   },
   methods: {
     reload() {
-      this.isLoading = true
-      this.$axios.get(ENDPOINT_RECIPE).then((resp) => {
-        const ids = resp.data.map((v) => v.id)
-        const selected = ids[Math.floor(Math.random() * ids.length)]
-        this.$axios.get(`${ENDPOINT_RECIPE}/${selected}`).then((resp) => {
-          this.selectedRecipe.id = resp.data.id
-          this.selectedRecipe.title = resp.data.title
-          this.selectedRecipe.ingredients = resp.data.ingredients
-          this.selectedRecipe.description = resp.data.description
-          this.selectedRecipe.source = resp.data.source
-          this.isLoading = false
-        })
-      })
+      this.isLoading = true;
+      axios.get(ENDPOINT_RECIPE).then(resp => {
+        const ids = resp.data.map(v => v.id);
+        const selected = ids[Math.floor(Math.random() * ids.length)];
+        axios.get(`${ENDPOINT_RECIPE}/${selected}`).then(resp => {
+          this.selectedRecipe.id = resp.data.id;
+          this.selectedRecipe.title = resp.data.title;
+          this.selectedRecipe.ingredients = resp.data.ingredients;
+          this.selectedRecipe.description = resp.data.description;
+          this.selectedRecipe.source = resp.data.source;
+          this.isLoading = false;
+        });
+      });
     }
   }
-}
+};
 </script>
 
 <style>
 body {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
 }
-
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -103,11 +103,9 @@ body {
   text-align: center;
   margin-top: 3em;
 }
-
 .content {
   max-width: 60em;
 }
-
 .title {
   display: block;
   font-weight: 300;
@@ -116,7 +114,6 @@ body {
   letter-spacing: 1px;
   margin-bottom: 0.2em;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 2.7em;
@@ -125,21 +122,18 @@ body {
   padding-bottom: 0.4em;
   margin-bottom: 1em;
 }
-
 .recipe-title {
   font-weight: 300;
   font-size: 1.5em;
   color: #35495e;
   padding: 0.2em;
 }
-
 .reload {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-    'Lucida Sans', Arial, sans-serif;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
   font-size: 2em;
   color: whitesmoke;
 }
-
 .reload-btn {
   margin: 3em;
   border: none;
@@ -148,16 +142,13 @@ body {
   width: 4em;
   background: #35495e;
 }
-
 .reload-btn:hover {
   background: whitesmoke;
   border: 0.01em solid #35495e;
 }
-
 .reload-btn:hover > * {
   color: #35495e;
 }
-
 table {
   margin: auto;
   margin-top: 1.3em;
@@ -167,12 +158,10 @@ table {
   font-size: 1.1em;
   width: 77%;
 }
-
 td {
   padding-bottom: 0.4em;
   padding-top: 0.4em;
 }
-
 .td-left {
   text-align: right;
   padding-right: 1em;
@@ -180,23 +169,19 @@ td {
   border-top-left-radius: 0.3em;
   border-bottom-left-radius: 0.3em;
 }
-
 .td-right {
   text-align: left;
   padding-left: 1em;
   border-top-right-radius: 0.3em;
   border-bottom-right-radius: 0.3em;
 }
-
 tr:nth-child(odd) {
   background-color: #f2f2f2;
 }
-
 p {
   font-size: 1.1em;
   padding-top: 0.5em;
 }
-
 .description {
   width: 77%;
   text-align: left;
